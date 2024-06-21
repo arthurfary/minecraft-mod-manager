617f51e71743ef0bf9f51fee8d3bf1b555020a5e
arthurfary <arthurfary22@gmail.com>
2024-06-20 21:56:14 -0300

    refactor

--- modSearch.js
+++ modSearch.js
@@ -1,4 +1,3 @@
-const axios = require("axios");
 const prompt = require("prompt-sync")();
 const { getDownloadLink, handleDownload } = require("./utils");
 
@@ -10,51 +9,45 @@ function displayModsOptions(mods) {
   console.log("---");
 }
 
-function getUserChoice() {
-  const answer = prompt(
-    "Choose a mod to download (1-10), or abort (a): "
-  );
-  return answer.trim();
-}
-
 function isValidChoice(choice, modsLength) {
   const parsedChoice = parseInt(choice);
   return parsedChoice >= 1 && parsedChoice <= modsLength;
 }
 
-async function handleChoice(choice, pageMods, version) {
-  const selectedMod = pageMods[parseInt(choice) - 1];
-  if (selectedMod) {
-    console.log(`   Project Title: ${selectedMod.title}`);
-    console.log(`   Project ID: ${selectedMod.project_id}`);
-    console.log(`   Downloads: ${selectedMod.downloads}`);
-    console.log(`   Categories: ${selectedMod.categories.join(", ")}`);
-    console.log(`   Versions: ${selectedMod.versions.join(", ")}`);
+async function handleChoice(choice, mods, version) {
+  const selectedMod = mods[parseInt(choice) - 1];
+  console.log(`   Project Title: ${selectedMod.title}`);
+  console.log(`   Project ID: ${selectedMod.project_id}`);
+  console.log(`   Downloads: ${selectedMod.downloads}`);
+  console.log(`   Categories: ${selectedMod.categories.join(", ")}`);
+  console.log(`   Versions: ${selectedMod.versions.join(", ")}`);
 
-    const shouldDownload = prompt("Download this mod? (Y/n): ");
-    // if is empty or Y
-    if (!shouldDownload || shouldDownload.toLowerCase() == "y") {
-      const downloadLink = await getDownloadLink(
-        selectedMod.project_id,
-        version
-      );
-      console.log("Downloading " + selectedMod.title);
-      handleDownload(downloadLink);
-    }
-  } else {
-    console.log("Invalid choice. Please try again.");
+  const shouldDownload = prompt("Download this mod? (Y/n): ");
+  // if is empty or Y
+  if (!shouldDownload || shouldDownload.toLowerCase() == "y") {
+    const downloadLink = await getDownloadLink(
+      selectedMod.project_id,
+      version
+    );
+    console.log("Downloading " + selectedMod.title);
+    handleDownload(downloadLink);
   }
 }
-
 
 async function displayMods(mods, version) {
   displayModsOptions(mods);
 
-  const choice = getUserChoice();
+  const choice = prompt(
+    "Choose a mod to download (1-10), or abort (a): "
+  ).trim();
 
-  if (choice === "a") {
+  // check for early abort
+  if (choice.toLowerCase() == 'a') {
     console.log("Aborted by user.");
-  } else if (isValidChoice(choice, mods.length)) {
+    return;
+  }
+
+  if (isValidChoice(choice, mods.length)) {
     await handleChoice(choice, mods, version);
   } else {
     console.log("Invalid choice. Please try again.");
